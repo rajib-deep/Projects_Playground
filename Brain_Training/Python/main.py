d5f8e3144e729b8bd10c100ea2877c012266705d
rajib-deep <rajib.deep@hotmail.com>
2025-07-26 18:43:20 +0530

    Adding codes

---
+++ Brain_Training/Python/main.py
@@ -0,0 +1,7 @@
+from Brain_Training.Python.passwordGenerator import passSugg1
+
+if __name__== "__main__":
+    print("!!Hello Coders!!")
+
+    print("We have a new password suggestion for you: ", end='')
+    passSugg1()
