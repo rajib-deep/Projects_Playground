44553cbd34c95f47734f7bb489a5a7cfa123d0c8
rajib-deep <71970240+rajib-deep@users.noreply.github.com>
2025-07-26 18:45:36 +0530

    Delete Brain_Training/Python directory

--- Brain_Training/Python/main.py
+++
@@ -1,7 +0,0 @@
-from Brain_Training.Python.passwordGenerator import passSugg1
-
-if __name__== "__main__":
-    print("!!Hello Coders!!")
-
-    print("We have a new password suggestion for you: ", end='')
-    passSugg1()
