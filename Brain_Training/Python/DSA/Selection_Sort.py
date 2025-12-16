21b77944c6b64d0c0eaaa9ec84dbed75978b5590
rajib-deep <rajib.deep@hotmail.com>
2025-12-16 11:44:14 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Brain_Training/Python/main.py
    #	Brain_Training/Python/passwordGenerator.py

--- Brain_Training/Python/DSA/Selection_Sort.py
+++ Brain_Training/Python/DSA/Selection_Sort.py
@@ -1,5 +1,20 @@
 
-def with_out_swap(arry):
+def without_swap(arry):
+    n=len(arry)
+    for i in range(n-1):
+        min_val_at_index=i
+        for j in range(i+1,n):
+            if arry[j] < arry[min_val_at_index]: # use > to sort in descending order
+                min_val_at_index = j
+        # inserting in the beginning and then swapping all elements one by one from insert point
+        # which is little slow
+        min_value=arry.pop(min_val_at_index)
+        arry.insert(i,min_value)
+        print(f"Array in step {i}:{arry}")
+
+    print(f"Sorted Array:{arry}")
+
+def with_swap(arry):
     n=len(arry)
     for i in range(n-1):
         min_val_at_index=i
@@ -7,13 +22,12 @@ def with_out_swap(arry):
             if arry[j] < arry[min_val_at_index]: # use > to sort in descending order
                 min_val_at_index = j
 
-        min_value=arry.pop(min_val_at_index)
-        arry.insert(i,min_value)
+        arry[i],arry[min_val_at_index]=arry[min_val_at_index],arry[i]
         print(f"Array in step {i}:{arry}")
 
     print(f"Sorted Array:{arry}")
 
-
 arry = [12,15,3,45,87,2,75,95,13]
 print(f"Array:{arry}")
-with_out_swap(arry)
+# without_swap(arry)
+without_swap(arry)
