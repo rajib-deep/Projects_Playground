ca439e476e4e13d7a7b9fcbe340ad3d1029cf4e1
rajib-deep <rajib.deep@hotmail.com>
2025-12-16 11:24:17 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Brain_Training/Python/main.py
    #	Brain_Training/Python/passwordGenerator.py

--- Brain_Training/Python/DSA/Bubble_Sort.py
+++ Brain_Training/Python/DSA/Bubble_Sort.py
@@ -1,11 +1,15 @@
-array1 = [12,15,3,45,87,2,75,95,13]
-print(f"Array:{array1}")
-n=len(array1)
+def bubble_sort(array):
+    n=len(arry)
+    for i in range(n-2): # n-2 as in the below loop last value will be already replaced.
+        print(f"Array in sorting step: {arry}")
+        for j in range(n-i-1):
+            if  arry[j] > arry[j+1]:
+                arry[j], arry[j+1] = arry[j+1], arry[j]
 
-for i in range(n-1): # n-1 as in the below loop last value will be already replaced.
-    print(f"Array in sorting step: {array1}")
-    for j in range(n-i-1):
-        if  array1[j] > array1[j+1]:
-            array1[j], array1[j+1] = array1[j+1], array1[j]
+    print(f"Sorted Array : {arry}")
 
-print(f"Sorted Array : {array1}")
+arry = [12,15,3,45,87,2,75,95,13]
+print(f"Array:{arry}")
+bubble_sort(arry)
+# Time complexity would be O(n^2) as after one loop, the array is looped through again and again n times.
+# This means there are n⋅n comparisons done in total, so the time complexity for Bubble Sort is: n^2
--- Brain_Training/Python/DSA/search_lowest_val.py
+++ Brain_Training/Python/DSA/search_lowest_val.py
@@ -1,4 +1,3 @@
-my_arr=[43,21,53,45,85,65,2,47,25,12,10,23] # although this is list in python
 #  we need to search the lowest value from the array
 
 def find_lowest(*args):
@@ -10,7 +9,8 @@ def find_lowest(*args):
             position = i
 
     print(f"Lowest value in the array is: {smallest} and position is : {position}")
-
+my_arr=[43,21,53,45,85,65,2,47,25,12,10,23] # although this is list in python
+print(f"Array: {my_arr}")
 find_lowest(*my_arr)
 
-# Time complexity would be O(n) as we eneed to traverse through n No of item.
+# Time complexity would be O(n) as we need to traverse through n No of item.
